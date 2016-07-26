882ff557fb325321df0bccdcc3a926a47b89f688
bouzuya <m@bouzuya.net>
2016-07-27 07:12:55 +0900

    improve test

--- test/index.ts
+++ test/index.ts
@@ -9,12 +9,33 @@ test('bath > path', () => {
   assert(path({ id: '123' }) === '/users/123');
 });
 
-test('bath > params > match', () => {
-  const { params } = bath('/users/:id');
-  assert.deepEqual(params('/users/123'), { id: '123' });
+test('bath > params > `/users`', () => {
+  const { params } = bath('/users');
+  assert.deepEqual(params('/'), undefined);
+  assert.deepEqual(params('/users'), {});
+  assert.deepEqual(params('/users/'), {}); // strict: false
+  assert.deepEqual(params('/users/123'), undefined);
+  assert.deepEqual(params('/users/123/'), undefined);
+  assert.deepEqual(params('/users/123/edit'), undefined);
+  assert.deepEqual(params('/USERS'), {}); // sensitive: false
+  assert.deepEqual(params('/USERS/'), {}); // strict: false && sensitive: false
+  assert.deepEqual(params('/USERS/123'), undefined);
+  assert.deepEqual(params('/USERS/123/'), undefined);
+  assert.deepEqual(params('/USERS/123/edit'), undefined);
 });
 
-test('bath > params > not match', () => {
-  const { params } = bath('/users');
-  assert.deepEqual(params('/users/123'), undefined);
+test('bath > params > `/users/:id`', () => {
+  const { params } = bath('/users/:id');
+  assert.deepEqual(params('/'), undefined);
+  assert.deepEqual(params('/users'), undefined);
+  assert.deepEqual(params('/users/'), undefined);
+  assert.deepEqual(params('/users/123'), { id: '123' });
+  assert.deepEqual(params('/users/123/'), { id: '123' }); // strict: false
+  assert.deepEqual(params('/users/123/edit'), undefined);
+  assert.deepEqual(params('/USERS'), undefined);
+  assert.deepEqual(params('/USERS/'), undefined);
+  assert.deepEqual(params('/USERS/123'), { id: '123' }); // sensitive: false
+  // strict: false && sensitive: false
+  assert.deepEqual(params('/USERS/123/'), { id: '123' });
+  assert.deepEqual(params('/USERS/123/edit'), undefined);
 });
