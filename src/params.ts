3fd1394a05b2c6120628e4617953dec9ae9bbd85
bouzuya <m@bouzuya.net>
2017-03-01 14:19:57 +0900

    v2 params

---
+++ src/params.ts
@@ -0,0 +1,87 @@
+interface Pattern {
+  pathPattern: RegExp;
+  parameters: Parameter[];
+}
+
+interface Parameter {
+  name: string;
+  pattern: RegExp | null;
+}
+
+const params = (
+  template: string,
+  params?: { [name: string]: RegExp; }
+): (pathname: string) => { [name: string]: string; } | undefined => {
+  const notFound = void 0;
+  const { pathPattern, parameters } = inputToPattern(template, params);
+  return (pathname: string): { [name: string]: string; } | undefined => {
+    const m = pathname.match(pathPattern);
+    if (m === null) return notFound;
+    const parameterValues = m.slice(1);
+    const results = parameterValues
+      .map((value, i) => {
+        const { name, pattern } = parameters[i];
+        return pattern === null || value.match(pattern) !== null
+          ? { [name]: value }
+          : notFound;
+      })
+      .filter((i) => i !== null)
+      .reduce((a, x) => Object.assign(a, x), {});
+    return parameterValues.length === Object.keys(results).length
+      ? results
+      : notFound;
+  };
+};
+
+// '/users/{id}', { 'id': /^\d+$/ }
+// -> { pathPattern: /^\/users\/([^\/]*)$/
+//    , parameters: [{ name: 'id', pattern: /^\d+$/ }]
+//    }
+const inputToPattern = (
+  template: string,
+  params: { [name: string]: RegExp; } | undefined
+): Pattern => {
+  const userParameterPatterns = ensureUserParameterPatterns(params);
+  const pathPattern = pathTemplateToPathPattern(template);
+  const parameterNames = pathTempalteToParameterNames(template);
+  const parameters = parameterNames.map((name) => {
+    const userPattern = userParameterPatterns.find(({ name: n }) => n === name);
+    const pattern = typeof userPattern === 'undefined'
+      ? null
+      : userPattern.pattern;
+    return { name, pattern };
+  });
+  return { pathPattern, parameters: parameters };
+};
+
+const ensureUserParameterPatterns = (
+  params: { [name: string]: RegExp; } | undefined
+): Parameter[] => {
+  if (typeof params === 'undefined') return [];
+  return Object
+    .keys(params)
+    .map((name) => {
+      const patternOrUndefined = params[name];
+      const pattern = typeof patternOrUndefined === 'undefined'
+        ? null
+        : patternOrUndefined;
+      return { name, pattern };
+    });
+};
+
+// '/users/{id}' -> ['id']
+const pathTempalteToParameterNames = (template: string): string[] => {
+  const nameMatcher = template.match(/\{[A-Za-z0-9_]+\}/g);
+  return nameMatcher === null
+    ? []
+    : nameMatcher.map((x) => x.substring(1, x.length - 1));
+};
+
+// '/users/{id}' -> /^\/users\/[^\/]*$/
+const pathTemplateToPathPattern = (template: string): RegExp => {
+  return new RegExp(
+    '^' + template.replace(/\{[A-Za-z0-9_]+\}/g, '([^\\/]*)') + '$'
+  );
+};
+
+export { params };
--- test/params.ts
+++ test/params.ts
@@ -1,13 +1,66 @@
 import * as assert from 'power-assert';
 import beater from 'beater';
-import { params } from '../src/';
+import { params } from '../src/params';
 
 const { test } = beater();
 
-test('params > match', () => {
-  assert.deepEqual(params('/users/:id')('/users/123'), { id: '123' });
+const category = 'params > ';
+
+test(category + 'returns function', () => {
+  const p = params('/users');
+  assert(typeof p === 'function');
 });
 
-test('params > not match', () => {
-  assert.deepEqual(params('/users')('/users/123'), undefined);
+test(category + 'template without parameters', () => {
+  const p = params('/users');
+  assert.deepEqual(p('/users'), {});
+  assert.deepEqual(p('/users/'), undefined);
+  assert.deepEqual(p('/users/123'), undefined);
+  assert.deepEqual(p('/users/123/'), undefined);
+  assert.deepEqual(p('/users/abc'), undefined);
+  assert.deepEqual(p('/users/abc/'), undefined);
+});
+
+test(category + 'template with parameter', () => {
+  const p = params('/users/{id}');
+  assert.deepEqual(p('/users'), undefined);
+  assert.deepEqual(p('/users/'), { id: '' });
+  assert.deepEqual(p('/users/123'), { id: '123' });
+  assert.deepEqual(p('/users/123/'), undefined);
+  assert.deepEqual(p('/users/abc'), { id: 'abc' });
+  assert.deepEqual(p('/users/abc/'), undefined);
+});
+
+test(category + 'template with strict parameter', () => {
+  const p = params('/users/{id}', { id: /^\d+$/ });
+  assert.deepEqual(p('/users'), undefined);
+  assert.deepEqual(p('/users/'), undefined);
+  assert.deepEqual(p('/users/123'), { id: '123' });
+  assert.deepEqual(p('/users/123/'), undefined);
+  assert.deepEqual(p('/users/abc'), undefined);
+  assert.deepEqual(p('/users/abc/'), undefined);
+});
+
+test(category + 'template with parameters', () => {
+  const p = params('/users/{userId}/posts/{id}');
+  assert.deepEqual(p('/users'), undefined);
+  assert.deepEqual(p('/users/'), undefined);
+  assert.deepEqual(p('/users/a'), undefined);
+  assert.deepEqual(p('/users/a/'), undefined);
+  assert.deepEqual(p('/users/a/posts/'), { userId: 'a', id: '' });
+  assert.deepEqual(p('/users/a/posts/1'), { userId: 'a', id: '1' });
+  assert.deepEqual(p('/users//posts/'), { userId: '', id: '' });
+});
+
+test(category + 'template with strict parameters', () => {
+  const p = params(
+    '/users/{userId}/posts/{id}',
+    { userId: /^\w+$/, id: /^\d+$/ }
+  );
+  assert.deepEqual(p('/users'), undefined);
+  assert.deepEqual(p('/users/'), undefined);
+  assert.deepEqual(p('/users/a'), undefined);
+  assert.deepEqual(p('/users/a/'), undefined);
+  assert.deepEqual(p('/users/a/posts/'), undefined);
+  assert.deepEqual(p('/users/a/posts/1'), { userId: 'a', id: '1' });
 });
