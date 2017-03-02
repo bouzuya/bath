85f5b048a9fa9772699035d9fc7728717eba5cd8
bouzuya <m@bouzuya.net>
2017-03-03 08:30:01 +0900

    add types

---
+++ src/types.ts
@@ -0,0 +1,32 @@
+// '/users/{userId}/messages/{messageId}'
+export type PathTemplate = string;
+
+// 'userId'
+export type ParameterName = string;
+
+// { 'userId': /^\w+$/, 'messageId': /^\d+$/ }
+export type ParameterPatterns = { [parameterName: string]: RegExp; };
+
+// { 'userId': 'john', 'messageId': '123' }
+export type Parameters = { [parameterName: string]: string; };
+
+// '/users/john/messages/123'
+export type Path = string;
+
+// assert.deepEqual(
+//   params('/users/john/messages/123'),
+//   { 'userId': 'john', 'messageId': '123' }
+// );
+export type ParametersFn = (path: Path) => Parameters | null;
+
+// assert.deepEqual(
+//   path({ 'userId': 'john', 'messageId': '123' }),
+//   '/users/john/messages/123'
+// );
+export type PathFn = (params: Parameters) => Path | null;
+
+// const { params, path } = bath('/users/{userId}/messages/{messageId}')
+export type Bath = (
+  template: PathTemplate,
+  patterns?: ParameterPatterns
+) => { path: PathFn, params: ParametersFn };
