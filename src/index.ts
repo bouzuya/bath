83e817f55958388f43b14233be0e364925bc0ac2
bouzuya <m@bouzuya.net>
2017-03-03 09:18:50 +0900

    fix index

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,12 @@
-import * as pathToRegexp from 'path-to-regexp';
+import { params as paramsFn } from './params';
+import { path as pathFn } from './path';
+import { Bath } from './types';
 
-type Path = string;
-type PathFn = (params?: Params) => Path;
-type Params = { [key: string]: string; };
-type ParamsFn = (path: Path) => Params | undefined;
-type Template = string;
-
-const bath = (template: Template): { path: PathFn; params: ParamsFn; } => {
-  const keys: pathToRegexp.Key[] = [];
-  const regexp = pathToRegexp(template, keys);
-  const compiled = pathToRegexp.compile(template);
-
-  const path: PathFn = (params?: Params): Path => {
-    return compiled(params);
-  };
-
-  const params: ParamsFn = (path: string): Params | undefined => {
-    const match = regexp.exec(path);
-    if (match === null) return void 0;
-    return match
-      .slice(1)
-      .map((v, i) => ({ [keys[i].name]: v }))
-      .reduce((a, x) => Object.assign(a, x), {});
-  };
-
+const bath: Bath = (template, patterns?) => {
+  const path = pathFn(template, patterns);
+  const params = paramsFn(template, patterns);
   return { path, params };
 };
 
-const path = (template: Template): PathFn => {
-  return bath(template).path;
-};
-
-const params = (template: Template): ParamsFn => {
-  return bath(template).params;
-};
-
 export default bath;
-export { bath, path, params };
+export { bath, pathFn as path, paramsFn as params };
